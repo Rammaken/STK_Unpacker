a2d7d4c5d1721df1d2bdb40b29eb22b62ef939c0
Rammaken <laggamer19573@gmail.com>
2022-08-06 10:03:52 -0430

    Added SoC file format support

--- STK_Unpacker/src/stk_unpacker/ui_unpacker.java
+++ STK_Unpacker/src/stk_unpacker/ui_unpacker.java
@@ -11,6 +11,7 @@ public class ui_unpacker extends javax.swing.JFrame {
     static String unpacked_path = ".";
     static String db_path = ".";
     static String app_version = "1.0";
+    static String file_type = "-xdb";
     public ui_unpacker() {
         initComponents();
     }
@@ -19,6 +20,7 @@ public class ui_unpacker extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        rdn_group = new javax.swing.ButtonGroup();
         background = new javax.swing.JPanel();
         bg_toolbar = new javax.swing.JPanel();
         btn_clear = new javax.swing.JButton();
@@ -35,8 +37,10 @@ public class ui_unpacker extends javax.swing.JFrame {
         btn_select_path2 = new javax.swing.JButton();
         bg_unpack = new javax.swing.JPanel();
         btn_unpack = new javax.swing.JButton();
-        box_game = new javax.swing.JComboBox<>();
         lbl_unpacked_path1 = new javax.swing.JLabel();
+        rdn_game_socru = new javax.swing.JRadioButton();
+        rdn_game_cscop = new javax.swing.JRadioButton();
+        rdn_game_socww = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("S.T.A.L.K.E.R. Database Unpacker");
@@ -57,7 +61,7 @@ public class ui_unpacker extends javax.swing.JFrame {
         btn_clear.setText("Clear");
         btn_clear.setBorderPainted(false);
         btn_clear.setContentAreaFilled(false);
-        btn_clear.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_clear.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btn_clear.setDefaultCapable(false);
         btn_clear.setFocusPainted(false);
         btn_clear.setFocusable(false);
@@ -81,7 +85,7 @@ public class ui_unpacker extends javax.swing.JFrame {
         btn_about1.setBorder(null);
         btn_about1.setBorderPainted(false);
         btn_about1.setContentAreaFilled(false);
-        btn_about1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_about1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btn_about1.setDefaultCapable(false);
         btn_about1.setFocusPainted(false);
         btn_about1.setFocusable(false);
@@ -106,7 +110,7 @@ public class ui_unpacker extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(app_logo)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btn_clear)
+                .addComponent(btn_clear, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btn_about1, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -115,11 +119,12 @@ public class ui_unpacker extends javax.swing.JFrame {
             bg_toolbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(bg_toolbarLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(bg_toolbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE, false)
-                    .addComponent(app_logo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(bg_toolbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(stalker_icon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btn_about1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btn_clear, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bg_toolbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(btn_about1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btn_clear, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(app_logo))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -144,7 +149,7 @@ public class ui_unpacker extends javax.swing.JFrame {
         btn_select_path1.setBorder(null);
         btn_select_path1.setBorderPainted(false);
         btn_select_path1.setContentAreaFilled(false);
-        btn_select_path1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_select_path1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btn_select_path1.setFocusPainted(false);
         btn_select_path1.setRolloverEnabled(false);
         btn_select_path1.addActionListener(new java.awt.event.ActionListener() {
@@ -158,7 +163,7 @@ public class ui_unpacker extends javax.swing.JFrame {
         btn_select_path2.setBorder(null);
         btn_select_path2.setBorderPainted(false);
         btn_select_path2.setContentAreaFilled(false);
-        btn_select_path2.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_select_path2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btn_select_path2.setFocusPainted(false);
         btn_select_path2.setRolloverEnabled(false);
         btn_select_path2.addActionListener(new java.awt.event.ActionListener() {
@@ -175,7 +180,7 @@ public class ui_unpacker extends javax.swing.JFrame {
         btn_unpack.setBorder(null);
         btn_unpack.setBorderPainted(false);
         btn_unpack.setContentAreaFilled(false);
-        btn_unpack.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_unpack.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btn_unpack.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 start_unpacking(evt);
@@ -195,12 +200,28 @@ public class ui_unpacker extends javax.swing.JFrame {
                 .addComponent(btn_unpack, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        box_game.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "CS/CoP", "SoC" }));
-        box_game.setOpaque(true);
-
         lbl_unpacked_path1.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
         lbl_unpacked_path1.setForeground(new java.awt.Color(255, 255, 255));
         lbl_unpacked_path1.setText("Game:");
+
+        rdn_game_socru.setBackground(new java.awt.Color(29, 29, 29));
+        rdn_group.add(rdn_game_socru);
+        rdn_game_socru.setForeground(new java.awt.Color(255, 255, 255));
+        rdn_game_socru.setText("SoC (RU)");
+        rdn_game_socru.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        rdn_game_cscop.setBackground(new java.awt.Color(29, 29, 29));
+        rdn_group.add(rdn_game_cscop);
+        rdn_game_cscop.setForeground(new java.awt.Color(255, 255, 255));
+        rdn_game_cscop.setText("CS/CoP");
+        rdn_game_cscop.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        rdn_game_socww.setBackground(new java.awt.Color(29, 29, 29));
+        rdn_group.add(rdn_game_socww);
+        rdn_game_socww.setForeground(new java.awt.Color(255, 255, 255));
+        rdn_game_socww.setSelected(true);
+        rdn_game_socww.setText("SoC (WW)");
+        rdn_game_socww.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
         javax.swing.GroupLayout backgroundLayout = new javax.swing.GroupLayout(background);
         background.setLayout(backgroundLayout);
@@ -209,32 +230,39 @@ public class ui_unpacker extends javax.swing.JFrame {
             .addComponent(bg_toolbar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(backgroundLayout.createSequentialGroup()
                 .addGap(22, 22, 22)
-                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(txt_unpacked_path, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lbl_unpacked_path)
-                    .addComponent(spt_db_path, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(txt_db_path, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 322, Short.MAX_VALUE)
-                    .addComponent(spt_unpacked_path)
-                    .addComponent(lbl_db_path)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, backgroundLayout.createSequentialGroup()
+                .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(backgroundLayout.createSequentialGroup()
                         .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lbl_unpacked_path1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(box_game, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(bg_unpack, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(rdn_game_socru)
+                            .addGroup(backgroundLayout.createSequentialGroup()
+                                .addComponent(rdn_game_socww)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(rdn_game_cscop))
+                            .addComponent(lbl_unpacked_path1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                        .addComponent(bg_unpack, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txt_unpacked_path, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(spt_db_path, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txt_db_path)
+                    .addGroup(backgroundLayout.createSequentialGroup()
+                        .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lbl_unpacked_path)
+                            .addComponent(lbl_db_path))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(spt_unpacked_path))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(btn_select_path2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btn_select_path1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(14, Short.MAX_VALUE))
+                .addContainerGap())
         );
         backgroundLayout.setVerticalGroup(
             backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(backgroundLayout.createSequentialGroup()
+                .addComponent(bg_toolbar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(backgroundLayout.createSequentialGroup()
-                        .addComponent(bg_toolbar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(lbl_db_path)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(txt_db_path, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -250,17 +278,24 @@ public class ui_unpacker extends javax.swing.JFrame {
                     .addComponent(btn_select_path2, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(spt_unpacked_path, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(backgroundLayout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(lbl_unpacked_path1)
+                        .addGap(4, 4, 4)
+                        .addGroup(backgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(rdn_game_socww)
+                            .addComponent(rdn_game_cscop))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(box_game, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(bg_unpack, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(71, 71, 71))
+                        .addComponent(rdn_game_socru)
+                        .addGap(61, 61, 61))
+                    .addGroup(backgroundLayout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(bg_unpack, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
 
-        getContentPane().add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 280));
+        getContentPane().add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 410, 290));
 
         getAccessibleContext().setAccessibleName("Unpacker");
 
@@ -268,8 +303,22 @@ public class ui_unpacker extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void start_unpacking(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_start_unpacking
+        
+        // If gamedata is CoP/CS
+        if(rdn_game_cscop.isSelected()) {
+            file_type = "-xdb";
+        }
+        // If gamedata is SoC WorldWide
+        if(rdn_game_socww.isSelected()) {
+            file_type = "-2947ww";
+        }
+        // If gamedata is SoC Russian
+        if(rdn_game_socru.isSelected()) {
+            file_type = "-2947ru";
+        }
+        
         // Generates the unpacker run file
-        String unpacker_data = "converter.exe -unpack -xdb " + "\"" + db_path + "\"" + " -dir " + "\"" + unpacked_path + "\"";
+        String unpacker_data = "converter.exe -unpack " + file_type + " \"" + db_path + "\"" + " -dir " + "\"" + unpacked_path + "\"";
 
         try {
             Runtime.getRuntime().exec("cmd.exe /c start cmd.exe /k \" start " + unpacker_data);
@@ -351,7 +400,6 @@ public class ui_unpacker extends javax.swing.JFrame {
     private javax.swing.JPanel background;
     private javax.swing.JPanel bg_toolbar;
     private javax.swing.JPanel bg_unpack;
-    private javax.swing.JComboBox<String> box_game;
     private javax.swing.JButton btn_about1;
     private javax.swing.JButton btn_clear;
     private javax.swing.JButton btn_select_path1;
@@ -360,6 +408,10 @@ public class ui_unpacker extends javax.swing.JFrame {
     private javax.swing.JLabel lbl_db_path;
     private javax.swing.JLabel lbl_unpacked_path;
     private javax.swing.JLabel lbl_unpacked_path1;
+    private javax.swing.JRadioButton rdn_game_cscop;
+    private javax.swing.JRadioButton rdn_game_socru;
+    private javax.swing.JRadioButton rdn_game_socww;
+    private javax.swing.ButtonGroup rdn_group;
     private javax.swing.JSeparator spt_db_path;
     private javax.swing.JSeparator spt_unpacked_path;
     private javax.swing.JLabel stalker_icon;
